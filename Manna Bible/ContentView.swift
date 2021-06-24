b5b1352ead39c938391ac32023c9f77eb5f8602c
Xander <roper.alexander@gmail.com>
2021-06-24 12:18:10 -0400

    Initial Commit

---
+++ Manna Bible/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Manna Bible
+//
+//  Created by Xander on 6/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Manna Bible/Manna_BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manna_BibleApp.swift
+//  Manna Bible
+//
+//  Created by Xander on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Manna_BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
